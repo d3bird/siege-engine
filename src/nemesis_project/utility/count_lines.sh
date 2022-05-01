aa61cdbbaab97cf49b470651af0392339c9a3714
Scott <dogbird9@gmail.com>
2022-05-01 19:25:21 -0400

    updated counting lines script

--- src/nemesis_project/utility/count_lines.sh
+++ src/nemesis_project/utility/count_lines.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
-cd ..
+echo "lines of code in the application"
+echo
+cd ../
+echo "lines in .h"
+find . -name '*.h' | xargs wc -l | grep total
+echo "lines in .hpp"
+find . -name '*.hpp' | xargs wc -l | grep total
+echo "lines in .cpp"
+find . -name '*.cpp' | xargs wc -l | grep total
+echo
+echo "lines of code in the entire project"
+echo
+cd ../
 echo "lines in .h"
 find . -name '*.h' | xargs wc -l | grep total
 echo "lines in .hpp"
