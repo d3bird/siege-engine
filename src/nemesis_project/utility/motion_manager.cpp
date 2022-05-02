40aa2b786d612e2e848b90ff657a9fa0b5d35d11
Scott <dogbird9@gmail.com>
2022-05-01 20:04:01 -0400

    added the framework for a motion manager

--- src/nemesis_project/utility/motion_manager.cpp
+++ src/nemesis_project/utility/motion_manager.cpp
@@ -1,5 +1,10 @@
 #include "motion_manager.h"
 
+motion_manger::motion_manger() {
+	OBJM = NULL;
+
+}
+
 motion_manger::motion_manger(optimized_spawner* objm){
 	OBJM = objm;
 }
@@ -9,14 +14,26 @@ motion_manger::~motion_manger()
 }
 
 
-void motion_manger::update_item_matrix(update_pak* up) {
+void motion_manger::update_item_matrix(update_pak& up) {
 
 }
 
 void motion_manger::push_updates() {
 
+	for (int i = 0; i < updates.size(); i++) {
+		update temp = updates[i];
+		if (temp.use_mat) {
+			OBJM->update_item_matrix(&temp.up, temp.mat);
+		}
+		else {
+			OBJM->update_item_matrix(&temp.up);
+		}
+	}
+	updates.clear();
 }
 
-void motion_manger::regester_sensor_net() {
+void motion_manger::regester_sensor_net(sensor_net& net) {
+
+
 
 }
--- src/nemesis_project/utility/motion_manager.h
+++ src/nemesis_project/utility/motion_manager.h
@@ -8,17 +8,28 @@
 class motion_manger
 {
 public:
+	motion_manger();
 	motion_manger(optimized_spawner* objm);
 	~motion_manger();
 
-	void update_item_matrix(update_pak* up);
+
+
+	void update_item_matrix(update_pak& up);
 	
 	void push_updates();
 	void regester_sensor_net(sensor_net& net);
 
 private:
 	optimized_spawner* OBJM;
+	static motion_manger* instance;
 
+	struct update {
+		update_pak up;
+		glm::mat4 mat;
+		bool use_mat;
+	};
+
+	std::vector< update> updates;
 
 };
 
