0a56099fcd4f53e831609940e2e613b3f88adaab
Scott <dogbird9@gmail.com>
2022-06-17 08:46:22 -0400

    fixed shader error
    graphics card needed the value there when the integrated graphics
    infered it to be there
    
    added statements to main that shoed opengl information

--- src/nemesis_project/main.cpp
+++ src/nemesis_project/main.cpp
@@ -71,6 +71,15 @@ int main() {
         std::cout << "something went wrong with the graphics engine" << std::endl;
     }
 
+    std::cout << "" << std::endl;
+    std::cout << "" << "OpenGL Vendor: " << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "" << "OpenGL Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "" << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "" << "OpenGL Shading Language Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+    std::cout << "" << std::endl;
+
+    //while (true);
+
     camera = API->get_camera();
     keys = API->get_keyboard();
     gui = API->get_engine_gui();
